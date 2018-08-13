b37f4603b95b3cea9da1abf9f94adc4505568143
Amélie Barré <amliebarre@gmail.com>
2018-08-13 13:43:04 +0200

    configure server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
 import * as logger from 'morgan';
 import * as helmet from 'helmet';
+import * as cors from 'cors';
 
 // Import routers
 
@@ -19,11 +20,26 @@ class Server {
     }
 
     public config() {
+        // Set up mongoose
+        const MONGO_URI = 'mongodb://localhost:27017/museo';
+        mongoose.connect(MONGO_URI || process.env.MONGODB_URI);
 
+        // Config
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json());
+        this.app.use(helmet());
+        this.app.use(logger('dev'));
+        this.app.use(compression());
+        this.app.use(cors());
     }
 
-    routes() {
+    public routes(): void {
+        let router: express.Router;
+        router = express.Router();
 
+        this.app.use('/', router);
+        // this.app.use('/api/artists', ArtistRouter);
     }
-
 }
+
+export default new Server().app;
