a9b0696d50708576480d0303fc283a1eaff375af
Amélie Barré <amliebarre@gmail.com>
2018-08-19 23:38:23 +0200

    GetArtist method in controller

--- src/controllers/artist-controller.ts
+++ src/controllers/artist-controller.ts
@@ -20,6 +20,28 @@ class ArtistController {
             }
         });
     }
+
+    /**
+     * Get artist by slug
+     * 
+     * @param req
+     * @param res
+     * @returns {Promise<void>}
+     * @constructor
+     */
+    public async GetArtist(req, res) {
+        await Artist.find({ slug: req.params.slug }, function(err, artist) {
+            try {
+                if (artist.length === 0) {
+                    res.status(500).send({status: 'erreur', message: 'Artiste inexistant'});
+                } else {
+                    res.status(200).send(artist);
+                }
+            } catch (e) {
+                res.status(500).send(e.error.message);
+            }
+        });
+    }
 }
 
 export default new ArtistController();
